fc481242e074eb9992b00d80a13f8e35b7f7a332
leo9iota <leo9iota@gmail.com>
2024-12-15 14:34:29 +0100

    Update Hono error handling

--- server/index.ts
+++ server/index.ts
@@ -5,24 +5,42 @@ import type { ErrorResponse } from '@/shared/types';
 
 const app = new Hono(); // Create new Hono instance
 
-app.get('/', (c) => {
-    return c.text('Hello Hono!');
+app.get('/', (ctx) => {
+    // throw new HTTPException(404, { message: 'Post not found', cause: { form: true } });
+    // throw new Error('Unexpected error');
+    return ctx.text('Hello Hono!');
 });
 
-app.onError((err, c) => {
+// Hono error handler
+app.onError((err, ctx) => {
     if (err instanceof HTTPException) {
         const errResponse =
             err.res ??
-            c.json<ErrorResponse>({
-                success: false,
-                error: err.message,
-                isFormError:
-                    err.cause && typeof err.cause === 'object' && 'form' in err.cause
-                        ? err.cause.form === true
-                        : false,
-            });
+            ctx.json<ErrorResponse>(
+                {
+                    success: false,
+                    error: err.message,
+                    isFormError:
+                        err.cause && typeof err.cause === 'object' && 'form' in err.cause
+                            ? err.cause.form === true
+                            : false,
+                },
+                err.status,
+            );
         return errResponse;
     }
+
+    // Handling unknown errors
+    return ctx.json<ErrorResponse>(
+        {
+            success: false,
+            error:
+                process.env.NODE_ENV === 'production'
+                    ? 'Internal Server Error' // Generic error message for production
+                    : (err.stack ?? err.message), // Return error stack, if not available, return error message
+        },
+        500,
+    );
 });
 
 export default app;

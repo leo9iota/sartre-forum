15e62575e071f6634012004dfd4ae7247ba34786
leo9iota <leo9iota@gmail.com>
2024-12-23 15:56:02 +0100

    Add login schema type

--- shared/types.ts
+++ shared/types.ts
@@ -1,18 +1,21 @@
-// Zod Schema and TS Types
+import { z } from 'zod';
 
 export type SuccessResponse<T = void> = {
     success: true;
     message: string;
 } & (T extends void ? {} : { data: T });
 
-// const data: SuccessResponse = {
-//     success: true,
-//     message: 'Post created',
-//     data: { id: 1 },
-// };
-
 export type ErrorResponse = {
     success: false;
     error: string;
     isFormError?: boolean;
-}
+};
+
+export const loginSchema = z.object({
+    username: z
+        .string()
+        .min(3)
+        .max(31)
+        .regex(/^[a-zA-Z0-9_]+$/),
+    password: z.string().min(3).max(255),
+});

8ae1d86c84ea887970251f32174b45844747ba03
leo9iota <leo9iota@gmail.com>
2025-04-20 17:30:38 +0200

    Add post and comment relations for upvotes

--- server/db/schemas/upvotes.ts
+++ server/db/schemas/upvotes.ts
@@ -1,5 +1,11 @@
+import { relations } from 'drizzle-orm';
 import { integer, pgTable, serial, text, timestamp } from 'drizzle-orm/pg-core';
 
+import { userTable } from './auth';
+import { commentsTable } from './comments';
+import { postsTable } from './posts';
+
+// Post table
 export const postUpvotesTable = pgTable('post_upvotes', {
     id: serial('id').primaryKey(),
     postId: integer('post_id').notNull(),
@@ -7,9 +13,38 @@ export const postUpvotesTable = pgTable('post_upvotes', {
     createdAt: timestamp('created_at', { withTimezone: true }).defaultNow().notNull(),
 });
 
+// Post relations
+export const postUpvoteRelations = relations(postUpvotesTable, ({ one }) => ({
+    post: one(postsTable, {
+        fields: [postUpvotesTable.postId],
+        references: [postsTable.id],
+        relationName: 'postUpvotes',
+    }),
+    user: one(userTable, {
+        fields: [postUpvotesTable.userId],
+        references: [userTable.id],
+        relationName: 'user',
+    }),
+}));
+
+// Comment table
 export const commentUpvotesTable = pgTable('comment_upvotes', {
     id: serial('id').primaryKey(),
     commentId: integer('comment_id').notNull(),
     userId: text('user_id').notNull(),
     createdAt: timestamp('created_at', { withTimezone: true }).defaultNow().notNull(),
 });
+
+// Comment relations
+export const commentUpvoteRelations = relations(commentUpvotesTable, ({ one }) => ({
+    post: one(commentsTable, {
+        fields: [commentUpvotesTable.commentId],
+        references: [commentsTable.id],
+        relationName: 'commentUpvotes',
+    }),
+    user: one(userTable, {
+        fields: [commentUpvotesTable.userId],
+        references: [userTable.id],
+        relationName: 'user',
+    }),
+}));

c81b1c436462e3053c6f134831ac0b66a2df9b51
leo9iota <leo9iota@gmail.com>
2025-04-20 16:21:25 +0200

    Add database schema for comments

---
+++ server/db/schemas/comments.ts
@@ -0,0 +1,13 @@
+import { integer, pgTable, serial, text, timestamp } from 'drizzle-orm/pg-core';
+
+export const commentsTable = pgTable('comments', {
+    id: serial().primaryKey(),
+    userId: text('user_id').notNull(),
+    postId: integer('post_id').notNull(),
+    parentCommentId: integer('parent_comment_id'),
+    content: text('content').notNull(),
+    createdAt: timestamp('created_at', { withTimezone: true }).defaultNow().notNull(),
+    depth: integer('depth').default(0).notNull(),
+    commentCount: integer('comment_count').default(0).notNull(),
+    points: integer('points').default(0).notNull(),
+});

340b266d2590e8cf7e1173d3b8f085d563064a23
leo9iota <leo9iota@gmail.com>
2025-05-12 22:23:38 +0200

    Implement post creation with schema validation

--- server/routes/posts.ts
+++ server/routes/posts.ts
@@ -1,11 +1,35 @@
 import { Hono } from 'hono';
 
+import { db } from '@/adapter';
 import { type Context } from '@/context';
+import { postsTable } from '@/db/schemas/posts';
 import { loggedIn } from '@/middleware/loggedIn';
 import { zValidator } from '@hono/zod-validator';
+
+import { createPostSchema, type SuccessResponse } from '@/shared/types';
 
 export const postRouter = new Hono<Context>().post(
     '/',
     loggedIn,
-    zValidator('form'),
+    zValidator('form', createPostSchema),
+    async (ctx) => {
+        // Define what we wanna do with the posts
+        const { title, url, content } = ctx.req.valid('form');
+        const user = ctx.get('user');
+        const [post] = await db
+            .insert(postsTable)
+            .values({
+                title,
+                content: content || null,
+                url: url || null,
+                userId: user!.id,
+            })
+            .returning({ id: postsTable.id });
+        return ctx.json<SuccessResponse<{ postId: number }>>({
+            success: true,
+            message: 'Post created',
+            data: { postId: post.id },
+        }, 201);
+    },
 );
+

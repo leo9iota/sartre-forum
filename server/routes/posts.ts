ccc7c49d3f7b6af071e358e40df277539d7e5b08
leo9iota <leo9iota@gmail.com>
2025-05-26 15:52:15 +0200

    Add implementation for upvote logic

--- server/routes/posts.ts
+++ server/routes/posts.ts
@@ -1,4 +1,5 @@
 import { Hono } from 'hono';
+import { HTTPException } from 'hono/http-exception';
 import { and, asc, countDistinct, desc, eq, sql } from 'drizzle-orm';
 
 import { db } from '@/adapter';
@@ -8,10 +9,13 @@ import { postsTable } from '@/db/schemas/posts';
 import { postUpvotesTable } from '@/db/schemas/upvotes';
 import { loggedIn } from '@/middleware/loggedIn';
 import { zValidator } from '@hono/zod-validator';
+import { z } from 'zod';
 
 import {
     createPostSchema,
     paginationSchema,
+    type PaginatedResponse,
+    type Post,
     type SuccessResponse,
 } from '@/shared/types';
 import { getISOFormatDateQuery } from '@/lib/utils';
@@ -100,4 +104,77 @@ export const postRouter = new Hono<Context>()
 
         // Fetch posts from database
         const posts = await postsQuery;
-    });
+
+        return ctx.json<PaginatedResponse<Post[]>>(
+            {
+                data: posts as Post[],
+                success: true,
+                message: 'Posts successfully fetched',
+                pagination: {
+                    page: page,
+                    totalPages: Math.ceil(count.count / limit) as number,
+                },
+            },
+            200,
+        );
+    })
+    .post(
+        '/:id/upvote',
+        loggedIn,
+        zValidator('param', z.object({ id: z.coerce.number() })),
+        async (ctx) => {
+            const { id } = ctx.req.valid('param');
+            const user = ctx.get('user')!;
+            let pointsChange: -1 | 1 = 1;
+
+            // Drizzle DB transaction for upvotes
+            const points = await db.transaction(async (tx) => {
+                const [existingUpvote] = await tx
+                    .select()
+                    .from(postUpvotesTable)
+                    .where(
+                        and(
+                            eq(postUpvotesTable.postId, id),
+                            eq(postUpvotesTable.userId, user.id),
+                        ),
+                    )
+                    .limit(1);
+
+                pointsChange = existingUpvote ? -1 : 1;
+
+                // Check if post has been updated
+                const [updated] = await tx
+                    .update(postsTable)
+                    .set({ points: sql`${postsTable.points} + ${pointsChange}` })
+                    .where(eq(postsTable.id, id))
+                    .returning({ points: postsTable.points });
+
+                // Check if post upvote failed, e.g. when the post doesn't exist
+                if (!updated) {
+                    throw new HTTPException(404, { message: 'Post not found' });
+                }
+
+                // Handle upvote deletion
+                if (existingUpvote) {
+                    await tx
+                        .delete(postUpvotesTable)
+                        .where(eq(postUpvotesTable.id, existingUpvote.id));
+                } else {
+                    await tx
+                        .insert(postUpvotesTable)
+                        .values({ postId: id, userId: user.id });
+                }
+
+                return updated.points;
+            });
+
+            return ctx.json<SuccessResponse<{ count: number; isUpvoted: boolean }>>(
+                {
+                    success: true,
+                    message: 'Post successfully updated',
+                    data: { count: points, isUpvoted: pointsChange > 0 },
+                },
+                200,
+            );
+        },
+    );

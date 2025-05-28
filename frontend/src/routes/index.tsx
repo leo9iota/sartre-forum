82124b7708b2e39674e3bfa4dee17a8ac4df6fa3
leo9iota <leo9iota@gmail.com>
2025-05-28 11:36:15 +0200

    Add example button

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import { createFileRoute } from '@tanstack/react-router';
 
+import { Button } from '@/components/ui/button';
+
 export const Route = createFileRoute('/')({
   component: HomeComponent,
 });
@@ -9,6 +11,7 @@ function HomeComponent() {
   return (
     <div className='p-2'>
       <h3>Welcome Home!</h3>
+      <Button>Click me!</Button>
     </div>
   );
 }

c2b0881af878beac103afb39aef10e14fda0e08f
leo9iota <leo9iota@gmail.com>
2025-05-28 10:21:58 +0200

    Apply formatting

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,14 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
+
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    TanStackRouterVite({
-      target: 'react',
-      autoCodeSplitting: true,
-    }),
-    react(),
-  ],
-})
+    plugins: [
+        TanStackRouterVite({
+            target: 'react',
+            autoCodeSplitting: true,
+        }),
+        react(),
+    ],
+});

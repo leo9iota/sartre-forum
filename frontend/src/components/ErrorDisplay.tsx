653b0edf4a0430dda31f4f4837f582e2e405ec9e
leo9iota <leo9iota@gmail.com>
2025-05-30 17:05:53 +0200

    Refactor `ErrorDisplay` component to accept an error prop and display an alert

--- frontend/src/components/ErrorDisplay.tsx
+++ frontend/src/components/ErrorDisplay.tsx
@@ -1,8 +1,17 @@
+import { AlertTriangleIcon } from 'lucide-react';
 
-function ErrorDisplay() {
+import { Alert } from './ui/alert';
+
+function ErrorDisplay({ error }: { error: Error }) {
   return (
-    <div>Error</div>
-  )
+    <div className='mt-8 flex items-center justify-center p-4'>
+      <div className='w-full max-w-md'>
+        <Alert variant='destructive'>
+          <AlertTriangleIcon />
+        </Alert>
+      </div>
+    </div>
+  );
 }
 
-export default ErrorDisplay
+export default ErrorDisplay;

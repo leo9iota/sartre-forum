73641c46a05303b2c4afd0fdfd93b455eabeab2d
leo9iota <leo9iota@gmail.com>
2025-06-01 18:04:03 +0200

    Refactor signup route to include form handling and validation

--- frontend/src/routes/signup.tsx
+++ frontend/src/routes/signup.tsx
@@ -1,15 +1,46 @@
 import { createFileRoute } from '@tanstack/react-router';
+import { useForm } from '@tanstack/react-form';
+import { fallback, zodSearchValidator } from '@tanstack/router-zod-adapter';
+
+import z from 'zod';
+
+import { loginSchema } from '@/shared/types';
+import { Card } from '@/components/ui/card';
+
+const signupSearchSchema = z.object({
+  redirect: fallback(z.string(), '/').default('/'),
+});
 
 export const Route = createFileRoute('/signup')({
   component: Signup,
+  validateSearch: zodSearchValidator(signupSearchSchema),
 });
 
 function Signup() {
-  return(
-    <div>
-      Hello World!
+  const search = Route.useSearch();
+  const form = useForm({
+    defaultValues: {
+      username: '',
+      password: '',
+    },
+    validators: {
+      onChange: loginSchema,
+    },
+  });
+
+  return (
+    <div className='w-full'>
+      <Card className='mx-auto mt-12 max-w-sm border-border/25'>
+        <form
+          onSubmit={(event) => {
+            event.preventDefault();
+            event.stopPropagation();
+            form.handleSubmit()
+          }}
+        ></form>
+      </Card>
     </div>
-  )
+  );
 }
 
 export default Signup;

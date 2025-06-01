fe7bc2eee6c3151d12f96f1ae7157f33443d1e06
leo9iota <leo9iota@gmail.com>
2025-06-01 17:12:25 +0200

    Remove unused form handling and validation from signup route

--- frontend/src/routes/signup.tsx
+++ frontend/src/routes/signup.tsx
@@ -1,55 +1,15 @@
 import { createFileRoute } from '@tanstack/react-router';
-import { useForm } from '@tanstack/react-form';
-import { fallback, zodSearchValidator } from '@tanstack/router-zod-adapter';
-
-import { z } from 'zod';
-
-import { loginSchema } from '@/shared/types';
-import { Card, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
-
-const signupSearchSchema = z.object({
-  redirect: fallback(z.string(), '/').default('/'),
-});
 
 export const Route = createFileRoute('/signup')({
   component: Signup,
-  validateSearch: zodSearchValidator(signupSearchSchema),
-  // beforeLoad: async ({ context, search }) => {
-  //   const user = await context.queryClient.ensureQueryData(userQueryOptions());
-  //   if (user) {
-  //     throw redirect({ to: search.redirect });
-  //   }
-  // },
 });
 
 function Signup() {
-  const form = useForm({
-    defaultValues: {
-      username: '',
-      password: '',
-    },
-    validators: {
-      onChange: loginSchema,
-    },
-  });
-  return (
-    <div className='w-full'>
-      <Card className='mx-auto mt-12 max-w-sm border-border/25'>
-        <form
-          onSubmit={(event) => {
-            event.preventDefault();
-            event.stopPropagation();
-            form.handleSubmit();
-          }}
-        >
-          <CardHeader>
-            <CardTitle className='text-center text-2xl'>Signup</CardTitle>
-            <CardDescription>
-              Enter your details below to create an account
-            </CardDescription>
-          </CardHeader>
-        </form>
-      </Card>
+  return(
+    <div>
+      Hello World!
     </div>
-  );
+  )
 }
+
+export default Signup;

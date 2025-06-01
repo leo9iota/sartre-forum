9e1d4efb0edb290434b40cc72852e59fbd7fa964
leo9iota <leo9iota@gmail.com>
2025-06-01 18:03:49 +0200

    Add `tsconfigPaths` plugin to Vite configuration

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,6 @@
 import path from 'path';
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
-
+import tsconfigPaths from 'vite-tsconfig-paths';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
@@ -14,6 +14,7 @@ export default defineConfig({
         }),
         react(),
         tailwindcss(),
+        tsconfigPaths()
     ],
     resolve: {
         alias: {

37d91dd548307f046ac715271b744cd57b4d40f9
leo9iota <leo9iota@gmail.com>
2025-06-06 13:17:33 +0200

    Export `insertPostSchema` from types for improved schema accessibility

--- shared/types.ts
+++ shared/types.ts
@@ -5,6 +5,9 @@ import { insertPostSchema } from '../server/db/schemas/posts';
 import type { ApiRoutes } from '../server/index';
 
 export { type ApiRoutes };
+export { insertPostSchema };
+
+
 
 export type SuccessResponse<T = void> = {
     success: true;

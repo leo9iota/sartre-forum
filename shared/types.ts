6bf34c69211cc70261f08103c7e8423aa20ccc37
leo9iota <leo9iota@gmail.com>
2025-06-27 12:24:45 +0200

    Fix typo

--- shared/types.ts
+++ shared/types.ts
@@ -38,7 +38,7 @@ export const createPostSchema = insertPostSchema
         content: true,
     })
     .refine((data) => data.url || data.content, {
-        message: 'Either URL or Content must be provided',
+        message: 'Either URL or content must be provided',
         path: ['url', 'content'],
     });
 

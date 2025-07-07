0595230565f058ea84ec3b1f5b5e33acee84e976
leo9iota <leo9iota@gmail.com>
2025-07-07 23:04:55 +0200

    Refactor form by swapping `FieldApi` generics for `AnyFieldApi` helper

--- frontend/src/components/FieldInfo.tsx
+++ frontend/src/components/FieldInfo.tsx
@@ -1,7 +1,6 @@
-import { FieldApi } from '@tanstack/react-form';
+import { AnyFieldApi } from '@tanstack/react-form';
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export function FieldInfo({ field }: { field: FieldApi<any, any, any, any> }) {
+export function FieldInfo({ field }: { field: AnyFieldApi }) {
   return (
     <>
       {field.state.meta.isTouched && field.state.meta.errors.length ? (

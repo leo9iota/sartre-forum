74ae4d66f6c0ca9b5bc430aa722b6794d322f133
leo9iota <leo9iota@gmail.com>
2025-12-30 16:20:53 +0100

    chore(root): update root configs for server rename

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'drizzle-kit';
 
 export default defineConfig({
     dialect: 'postgresql',
-    schema: 'apps/api/db/schemas/*',
+    schema: 'apps/server/db/schemas/*',
     out: 'drizzle',
     dbCredentials: {
         url: process.env['DATABASE_URL']!

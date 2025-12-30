0d2347db91903fb01aaf6c80bd50e11edae302c2
leo9iota <leo9iota@gmail.com>
2025-12-30 17:18:07 +0100

    refactor(config): move Drizzle config into `server/` dir

--- apps/server/drizzle.config.ts
+++ apps/server/drizzle.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from 'drizzle-kit';
 
 export default defineConfig({
     dialect: 'postgresql',
-    schema: 'apps/server/db/schemas/*',
-    out: 'drizzle',
+    schema: './db/schemas/*',
+    out: './drizzle',
     dbCredentials: {
         url: process.env['DATABASE_URL']!
     }

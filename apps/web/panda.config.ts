fb5f9d26bd7378f2190ef17152e30ea3c2da9be8
leo9iota <leo9iota@gmail.com>
2026-01-02 19:37:22 +0100

    feat(web): setup park ui and panda css

--- apps/web/panda.config.ts
+++ apps/web/panda.config.ts
@@ -2,6 +2,22 @@ import { defineConfig } from '@pandacss/dev';
 import { createPreset } from '@park-ui/panda-preset';
 import neutral from '@park-ui/panda-preset/colors/neutral';
 
+import { animationStyles } from '@/theme/animation-styles';
+import { green } from '@/theme/colors/green';
+import { jade } from '@/theme/colors/jade';
+import { red } from '@/theme/colors/red';
+import { slate } from '@/theme/colors/slate';
+import { conditions } from '@/theme/conditions';
+import { globalCss } from '@/theme/global-css';
+import { keyframes } from '@/theme/keyframes';
+import { layerStyles } from '@/theme/layer-styles';
+import { recipes, slotRecipes } from '@/theme/recipes';
+import { textStyles } from '@/theme/text-styles';
+import { colors } from '@/theme/tokens/colors';
+import { durations } from '@/theme/tokens/durations';
+import { shadows } from '@/theme/tokens/shadows';
+import { zIndex } from '@/theme/tokens/z-index';
+
 export default defineConfig({
     // Whether to use css reset
     preflight: true,
@@ -26,5 +42,87 @@ export default defineConfig({
     jsxFramework: 'solid',
 
     // The output directory for your css system
-    outdir: 'styled-system'
+    outdir: 'styled-system',
+
+    globalCss: globalCss,
+    conditions: conditions,
+
+    theme: {
+        extend: {
+            animationStyles: animationStyles,
+            recipes: recipes,
+            slotRecipes: slotRecipes,
+            keyframes: keyframes,
+            layerStyles: layerStyles,
+            textStyles: textStyles,
+
+            tokens: {
+                colors: colors,
+                durations: durations,
+                zIndex: zIndex
+            },
+
+            semanticTokens: {
+                colors: {
+                    fg: {
+                        default: {
+                            value: {
+                                _light: '{colors.gray.12}',
+                                _dark: '{colors.gray.12}'
+                            }
+                        },
+
+                        muted: {
+                            value: {
+                                _light: '{colors.gray.11}',
+                                _dark: '{colors.gray.11}'
+                            }
+                        },
+
+                        subtle: {
+                            value: {
+                                _light: '{colors.gray.10}',
+                                _dark: '{colors.gray.10}'
+                            }
+                        }
+                    },
+
+                    border: {
+                        value: {
+                            _light: '{colors.gray.4}',
+                            _dark: '{colors.gray.4}'
+                        }
+                    },
+
+                    error: {
+                        value: {
+                            _light: '{colors.red.9}',
+                            _dark: '{colors.red.9}'
+                        }
+                    },
+
+                    jade: jade,
+                    gray: slate,
+                    red: red,
+                    green: green
+                },
+
+                shadows: shadows,
+
+                radii: {
+                    l1: {
+                        value: '{radii.xs}'
+                    },
+
+                    l2: {
+                        value: '{radii.sm}'
+                    },
+
+                    l3: {
+                        value: '{radii.md}'
+                    }
+                }
+            }
+        }
+    }
 });
---
+++ apps/web/src/theme/recipes/index.ts
@@ -0,0 +1,12 @@
+import { absoluteCenter } from './absolute-center';
+import { button } from './button';
+import { group } from './group';
+import { spinner } from './spinner';
+
+export const recipes = {
+    button,
+    group,
+    absoluteCenter,
+    spinner
+};
+export const slotRecipes = {};

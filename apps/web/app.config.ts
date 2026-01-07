c958c46dcacf1c91053a9b47c4e39ce42848df1d
leo9iota <leo9iota@gmail.com>
2026-01-07 22:08:42 +0100

    chore: update dependencies and configuration

--- apps/web/app.config.ts
+++ apps/web/app.config.ts
@@ -3,13 +3,14 @@ import { resolve } from 'path';
 import { defineConfig } from '@solidjs/start/config';
 
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
     server: {
         preset: 'bun'
     },
     vite: {
-        plugins: [vanillaExtractPlugin()],
+        plugins: [tsconfigPaths(), vanillaExtractPlugin()],
         resolve: {
             alias: {
                 '@': resolve(import.meta.dirname, './src')

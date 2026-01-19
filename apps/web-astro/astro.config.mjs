6ecd25ef84128b2987acd539189a6ef3e1e35fab
leo9iota <leo9iota@gmail.com>
2026-01-19 22:32:54 +0100

    feat(web-astro): enable hybrid output for mixed static/server rendering

--- apps/web-astro/astro.config.mjs
+++ apps/web-astro/astro.config.mjs
@@ -1,10 +1,12 @@
 import path from 'node:path';
+
 import node from '@astrojs/node';
 import solid from '@astrojs/solid-js';
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
+    output: 'hybrid',
     integrations: [solid()],
     adapter: node({
         mode: 'standalone'
